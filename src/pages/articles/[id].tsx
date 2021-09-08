22a7457c9f8820f2c8900b357266cf04353e1d5a
yk1979 <yukiirisawa1979@gmail.com>
2021-09-08 17:59:06 +0900

    getStaticProps, getStaticPaths

--- src/pages/articles/[id].tsx
+++ src/pages/articles/[id].tsx
@@ -1,9 +1,14 @@
 import client from "@/utils/client";
 import styles from "@/styles/Home.module.css";
-import { GetServerSideProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { useRouter } from "next/dist/client/router";
 
-export const getServerSideProps: GetServerSideProps = async ({ query }) => {
-  const contentId = query.id;
+export const getStaticPaths: GetStaticPaths = async () => {
+  return { paths: [], fallback: true };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const contentId = params?.id;
   if (typeof contentId !== "string") {
     throw new Error("invalid query");
   }
@@ -15,7 +20,13 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   return { props: { data } };
 };
 
-export default function Home({ data }: any) {
+export default function ArticleDetail({ data }: any) {
+  const router = useRouter();
+  // getStaticPaths で paths の初期値をを空配列にしたのでこれが必要になる
+  // 初回のリクエストでは fallback でこれが表示される（裏で getStaticProps が実行されている）
+  // 2回目以降のリクエストでは、初回りクエストの裏で生成されたHTMLがレンダリングされる
+  if (router.isFallback) return <div>loading</div>;
+
   return (
     <div className={styles.container}>
       <h1>{data.title}</h1>
--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -1,21 +1,34 @@
 import client from "@/utils/client";
+import Link from "next/link";
 import styles from "@/styles/Home.module.css";
+import { GetStaticProps } from "next";
 
-export const getServerSideProps = async () => {
+// export const getServerSideProps = async () => {
+//   const data = await client.get({
+//     endpoint: "articles",
+//   });
+//   return { props: { data } };
+// };
+
+export const getStaticProps: GetStaticProps = async () => {
   const data = await client.get({
     endpoint: "articles",
   });
-  return { props: { data } };
+  // ビルド時に実行されるので、リクエストするたびに console.log が実行されるわけではない
+  console.log("hello");
+  // revalidate をつけておくと、指定の秒数が経過した後にリクエストがあるともう一度ページを作り直してくれる
+  // これが incremental regeneration
+  return { props: { data }, revalidate: 10 };
 };
 
-export default function Home({ data }: any) {
+export default function Articles({ data }: any) {
   const { contents } = data;
   return (
     <div className={styles.container}>
       <ul>
         {contents.map(({ id, title }: any, index: number) => (
           <li key={index}>
-            <a href={`/articles/${id}/`}>{title}</a>
+            <Link href={`/articles/${id}/`}>{title}</Link>
           </li>
         ))}
       </ul>
